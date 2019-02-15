65156e61acdc3842ece3be8e9c01409b7a132912
Infinical <inficalvin@gmail.com>
2019-02-15 11:25:34 +0300

    step 9

--- lib/person.rb
+++ lib/person.rb
@@ -1,26 +1,44 @@
 class Person
+  attr_accessor :name
+  attr_accessor :cash
+  attr_accessor :account
 
-    attr_accessor :name
-    attr_accessor :cash
-    attr_accessor :account
+  def initialize(attrs = {})
+    @name = set_name(attrs[:name])
+    @cash = 0
+    @account = nil
+  end
 
-    def initialize(attrs = {})
-        @name = set_name(attrs[:name])
-        @cash = 0
-        @account = nil
-    end
-    def create_account
-        @account = Account.new(owner:self)
-    end
-    def deposit(amount)
-        @account = Account.new(owner:self)
-    end
+  def create_account
+    @account = Account.new(owner: self)
+  end
 
-    private
-    def set_name(obj)
-        obj == nil ? missing_name : @owner =obj
+  def deposit(amount)
+    if @account
+      deposit_amount(amount)
+    else
+      missing_account
     end
-    def missing_name
-        raise "A name is required"
-    end
+  end
+  def withdraw()
+  end
+
+  private
+
+  def set_name(obj)
+    obj.nil? ? missing_name : @owner = obj
+  end
+
+  def deposit_amount(amount)
+    @cash -= amount
+    account.balance += amount
+  end
+
+  def missing_name
+    raise 'A name is required'
+  end
+
+  def missing_account
+    raise 'No account present'
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,37 +4,47 @@ require './lib/account'
 require 'date'
 
 describe Person do
+  subject { Person.new(name: 'Thomas') }
 
-    subject {Person.new(name: 'Thomas')}
-
-    it 'is expected to have :name on initialize'do
+  it 'is expected to have :name on initialize' do
     expect(subject.name).not_to be nil
-    end
-    it 'is expected to raise an error if no name is set'do
-    expect{Person.new}.to raise_error 'A name is required'
-    end
-    it 'is expected to have :cash attribute with value 0 on initialize'do
+  end
+  it 'is expected to raise an error if no name is set' do
+    expect { Person.new }.to raise_error 'A name is required'
+  end
+  it 'is expected to have :cash attribute with value 0 on initialize' do
     expect(subject.cash).to eq 0
-    end
-    it 'is expected to have a :account attribute'do
+  end
+  it 'is expected to have a :account attribute' do
     expect(subject.account).to be nil
-    end
-    describe 'can create an Account' do
-    before {subject.create_account}
+  end
+  describe 'can create an Account' do
+    before { subject.create_account }
 
-    it 'of Account class'do
-    expect(subject.account).to be_an_instance_of Account
+    it 'of Account class' do
+      expect(subject.account).to be_an_instance_of Account
     end
-    it 'with himself as an owner'do
-    expect(subject.account.owner).to be subject
+    it 'with himself as an owner' do
+      expect(subject.account.owner).to be subject
     end
+  end
+  describe 'can manage funds if an account has been created' do
+    let(:atm) { Atm.new }
+    before { subject.create_account }
+
+    it 'can deposit funds' do
+      expect(subject.deposit(100)).to be_truthy
     end
-    describe 'can manage funds if an account has been created'do
-    let(:atm){Atm.new}
-    before {subject.create_account}
-    it 'can deposit funds'do
-    expect(subject.deposit(100)).to be_truthy
+    it 'funds are added to the account balance - deducted from cash' do
+      subject.cash = 100
+      subject.deposit(100)
+      expect(subject.account.balance).to be 100
+      expect(subject.cash).to be 0
     end
-    
+  end
+  describe 'can not manage funds if no account been created' do
+    it 'can\'t deposit funds' do
+      expect { subject.deposit(100) }.to raise_error(RuntimeError, 'No account present')
     end
+  end
 end
